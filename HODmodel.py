6a3f471f2b7c61ddc80b872ef475c66ef1ac4dbc
Abhishek Jana <ajana@localhost.localdomain>
2019-09-15 17:04:21 -0500

    updated

--- HODmodel.py
+++ HODmodel.py
@@ -13,17 +13,17 @@ class Occupy:
         self.alpha = HODpar["alpha"]
          
     def central(self):
-        N_cen = erfc(np.log(self.M_cut/self.M)/(np.sqrt(2)*self.sigma))/2.0
-        return N_cen
-         
+        _Ncen = 0.5*erfc(np.log(10**self.M_cut/self.M)/(np.sqrt(2)*self.sigma))
+        return np.random.binomial(1,_Ncen)
+        
     def satellite(self):
-        N_sat = Occupy.central(self)*((self.M - self.kappa*self.M_cut)/self.M1)**self.alpha
-        return N_sat
+        _Nsat = Occupy.central(self)*((self.M - self.kappa*(10**self.M_cut))/10**self.M1)**self.alpha
+        return np.random.poisson(_Nsat)
          
 par = {"M_cut": 13., "sigma": 0.98, "kappa": 1.13 , "M1": 14., "alpha" : .9}
 
 def main():
-    occupy = Occupy(10,par)
+    occupy = Occupy(1e15,par)
     print (occupy.central())
     print (occupy.satellite())
     
