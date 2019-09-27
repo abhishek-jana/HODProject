6209f999b9b9a953092e4dc6dad8ee2a2891e167
Abhishek Jana <ajana@ksu-wireless-7-157.campus.ksu.edu>
2019-09-27 13:11:51 -0500

    updated

--- HODmodelv2.py
+++ HODmodelv2.py
@@ -1,5 +1,13 @@
+### NOT WORKING ###
+
+
 import numpy as np
 from scipy.special import erfc
+import multiprocessing as mp
+from joblib.externals.loky import set_loky_pickler
+from joblib import parallel_backend
+from joblib import Parallel, delayed
+from joblib import wrap_non_picklable_objects
 
 filename = "/mnt/data1/MDhalos.npy"
  
@@ -51,31 +59,11 @@ class Coordinates(Occupy):
     def __init__(self,HODpar,fin): 
         super().__init__(HODpar,fin)
 
-    def nfw_profile(self,u,r_s):
-        """
-        Input:
-        ------
-        u : floot, drawn from a uniform distribution
-        r_s : Scale radius.
-
-        Output:
-        ------
-        Returns a flooting point following nfw profile.
-        Details - https://en.wikipedia.org/wiki/Navarro-Frenk-White_profile
-        """
-        _coeff = [1./(r_s**3) , 2./(r_s**2) , 1./r_s , -1./u]
-        roots = np.roots(_coeff)
-        real = [i for i in roots if np.isreal(i)][0]
-        return real
-
-    def sphere_coordinates(self,number_of_particles,R,r_s):
+    def sphere_coordinates(self,number_of_particles,R):
         """
         Given the number of particles this will generate random uniform points inside a sphere of radius R
         """
-        vec_profile = np.vectorize(Coordinates.nfw_profile)
         u = np.random.uniform(0.,1., (number_of_particles,1))
-        u = vec_profile(self,u,r_s)
-        u = np.real(u)
         theta = np.arccos(1-2*np.random.uniform(0.,1.,(number_of_particles,1)))
         phi = np.random.uniform(0.0,1.,(number_of_particles,1))*2*np.pi
         x = np.cbrt(u)*R*np.sin( theta ) * np.cos( phi )
@@ -83,7 +71,7 @@ class Coordinates(Occupy):
         z = np.cbrt(u)*R*np.cos( theta)
         u,theta,phi = [None,None,None]
         return np.c_[x,y,z]
-        
+
     def cen_coord(self):
         """
         Returns the coordinates of the central galaxies
@@ -97,15 +85,15 @@ class Coordinates(Occupy):
         xcen,ycen,zceni,__nonzero = [None,None,None,None]
         return _cen
     
-    def sat_coord(self):
+    def sat_coord(self,n_jobs = 1):
         """
         Returns the coordinates of the satellite galaxies.
         Change the radius to Mpc
         """
+        backend = 'loky'
         _sat = Occupy.satellite(self)
         __nonzero = _sat.nonzero()
-        virial_radius = np.take(self.fout["r2"], __nonzero)/1000.
-        scale_radius = np.take(self.fout["r1"],__nonzero)/1000.
+        radius = np.take(self.fout["r1"],__nonzero)/1000.
         xsat = np.take(self.fout["x"],__nonzero)
         ysat = np.take(self.fout["y"],__nonzero)
         zsat = np.take(self.fout["z"],__nonzero)
@@ -113,29 +101,28 @@ class Coordinates(Occupy):
         xyz_sat = np.vstack([xsat,ysat,zsat]).T
         xyz_sat = np.repeat(xyz_sat,_sat[0],axis=0)
         xsat,ysat,zsat = [None,None,None]
-        xyz = [Coordinates.sphere_coordinates(self,i,j,k) for i,j,k in zip(_sat,virial_radius,scale_radius)]
+        results = Parallel(n_jobs,backend=backend)(delayed(Coordinates.sphere_coordinates)(self,i,j) for i,j in zip(_sat[0],radius[0]))
         radius,_sat,__nonzero = [None,None,None]
-        return np.vstack((xyz)) + xyz_sat
+        return np.vstack((results)) + xyz_sat
     
-    def galaxy_coordinates(self):
+    def galaxy_coordinates(self,n_jobs = 1):
         """
         Returns the combined galaxy coordinates of satellite and central galaxies
         """
-        return np.vstack((Coordinates.cen_coord(self),Coordinates.sat_coord(self)))
+        return np.vstack((Coordinates.cen_coord(self),Coordinates.sat_coord(self,n_jobs = 1)))
          
 par = {"M_cut": 13., "sigma": 0.98, "kappa": 1.13 , "M1": 14., "alpha" : .9}
 import time
 def main():
     np.random.seed(42)
     occupy = Coordinates(par,filename)
+    print (np.max(occupy.fout["x"]))
     #sat = occupy.satellite()
     #r = np.take(occupy.fout["r1"],sat.nonzero())
     #sat = np.take(sat,sat.nonzero())
     tic = time.time()
-    #print (occupy.sat_coord().shape)
-    print (occupy.sphere_coordinates(10,occupy.fout["r2"][0]/1000.,occupy.fout["r1"][0]/1000.))
-    print (occupy.fout["r1"][0]/1000.,occupy.fout["r2"][0]/1000.) 
-    #print (np.save('output_file.npy',occupy.galaxy_coordinates()))
+    #xyz_sat = Parallel(n_jobs=-1)(delayed(occupy.sphere_coordinates)(i,j) for i,j in zip(sat[0],r[0])) 
+    print (occupy.sat_coord(1)[0])
     #print (xyz_sat.shape)
     print (f'Total time = {time.time()-tic}')
 if __name__ == "__main__":
